4d0f4e5f8a68b05f38bee066f6ff58a05b3246f7
adriangasinski <adrian@datahacking.tech>
2019-05-03 12:21:54 +0200

    Add first controller (HomeController). Add first view (Home/Index.cshtml). Add layout file and ViewStart. Add ViewModel for View Home.

---
+++ Dh.Courses.CarApp/Dh.Courses.CarApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Dh.Courses.CarApp.Models;
+using Dh.Courses.CarApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Dh.Courses.CarApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICarRepository _carRepository;
+
+        public HomeController(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Przegląd samochodów";
+            var cars = _carRepository.GetAllCars().OrderBy(s => s.Company);
+
+            var homeVM = new HomeVM()
+            {
+                Title = "Przegląd samochodów",
+                Cars = cars.ToList()
+            };
+
+            return View(homeVM);
+        }
+    }
+}
---
+++ Dh.Courses.CarApp/Dh.Courses.CarApp/ViewModels/HomeVM.cs
@@ -0,0 +1,11 @@
+﻿using Dh.Courses.CarApp.Models;
+using System.Collections.Generic;
+
+namespace Dh.Courses.CarApp.ViewModels
+{
+    public class HomeVM
+    {
+        public string Title { get; set; }
+        public List<Car> Cars { get; set; }
+    }
+}
